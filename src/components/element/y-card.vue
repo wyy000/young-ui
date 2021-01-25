526a5111653f94d94564e5b01c7c584383629a57
yang <952711716@qq.com>
2021-01-25 19:45:45 +0800

    feat:添加spinner,card组件

---
+++ src/components/element/y-card.vue
@@ -0,0 +1,4 @@
+<template lang="pug">
+div(class="p-4 rounded-md bg-white shadow hover:shadow-lg transform hover:-translate-x-10")
+  slot
+</template>
---
+++ src/components/element/y-spinner.vue
@@ -0,0 +1,4 @@
+<template lang="pug">
+svg(viewBox="0 0 24 24" fill="currentColor" class="animate-spin")
+  path(d="M12 3a9 9 0 0 1 9 9h-2a7 7 0 0 0-7-7V3z")
+</template>
