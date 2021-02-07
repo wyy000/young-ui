189f8182da3fc20964f0db8b31d68f1579206eeb
yang <952711716@qq.com>
2021-02-07 20:35:45 +0800

    feat:添加select组件

--- src/components/doc/y-search.vue
+++ src/components/doc/y-search.vue
@@ -1,5 +1,9 @@
 <template lang="pug">
-a-text-input(v-model="keyword" class="w-80 text-white border border-white rounded-md bg-emerald-500 focus:bg-white focus:ring-0 focus:border-white focus:text-coolGray-500")
+div(class="rounded-md bg-emerald-500 flex")
+  span(class="w-16 border border-white border-r-0 rounded-l-md flex items-center")
+    svg(fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6 mx-auto")
+      path(stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z")
+  a-text-input(v-model="keyword" class="w-80 text-white border-white border-l-0 rounded-r-md bg-emerald-500 focus:ring-0 focus:bg-white focus:border-white focus:text-coolGray-500")
 </template>
 
 <script>
