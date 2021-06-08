0cfb7ea8f34349f07d8de3ddb060430667531a8a
wyy000 <sxd_wangyangyang@csmail.citicbank.com>
2021-06-08 17:21:18 +0800

    feat: 看板组件修改

--- src/views/documents/signage.vue
+++ src/views/documents/signage.vue
@@ -1,19 +1,44 @@
 <template lang="pug">
-div(class="w-full h-2/3h flex space-x-4")
-  y-card(class="h-full flex-1 flex flex-col")
+//div(class="w-full h-1/2h mb-4 flex flex-wrap space-x-4")
+  y-card(class="h-full w-1/2 flex-grow flex flex-col" style="flex-basis: 40%;")
     section.p-1
       h1 Signage Demo
     signage-charts(:data="data" class="flex-1")
-  y-card(class="flex-1 h-full flex flex-col")
+  y-card(class="h-full w-1/2 flex-grow flex flex-col" style="flex-basis: 40%;")
     section.p-1
       h1 ChartAxis
     div(class="flex-1 flex" ref="axisEl")
       svg(class="p-0 flex-1 border" ref="axisSvgEl")
         chart-axis(:width="axisInfo.width" :height="axisInfo.height" :x="axisInfo.x"  :y="axisInfo.y" :xRange="axisInfo.xRange" :yRange="axisInfo.yRange")
+div(class="w-full h-full flex flex-wrap space-x-4")
+  y-card(class="w-full flex-shrink-0 flex flex-col space-y-2 overflow-auto" style="height: 80vh;")
+    div.w-full.h-full.overflow-auto.flex.flex-col
+      section.p-1.flex.flex-col.text-coolGray-700
+        h1 ChartAxis
+        span 描述
+        div.flex.justify-between
+          button TYPE
+          button DATE
+      transition(
+        enter-active-class="transition-all duration-500 ease-in-out"
+        leave-active-class="transition-all duration-500 ease-in-out"
+        @before-enter="el => {el.style.height = '0px'}"
+        @enter="el => {el.style.height = el.children[0].offsetHeight + 'px'; }"
+        @leave="el => {el.style.height = '0px'}"
+      )
+        section(v-if="showConditions" class="overflow-hidden")
+          div
+            h2 conditions
+      div(class="flex justify-center")
+        svg(class="h-6 w-12 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" @click="showConditions = !showConditions")
+          path(stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7")
+      div(class="flex-1 flex" ref="axisEl" style="min-height: 50vh")
+        svg(class="p-0 flex-1 border" ref="axisSvgEl")
+          chart-axis(:width="axisInfo.width" :height="axisInfo.height" :x="axisInfo.x"  :y="axisInfo.y" :xRange="axisInfo.xRange" :yRange="axisInfo.yRange")
 </template>
 
 <script>
-import {defineComponent, reactive, ref, toRefs, onUpdated, onMounted} from 'vue'
+import {defineComponent, reactive, ref, toRefs, onUpdated, onMounted, watch, nextTick} from 'vue'
 
 import YCard from '@/components/element/y-card.vue'
 import SignageCharts from '@/components/signage/signage-charts.vue'
@@ -41,6 +66,7 @@ export default defineComponent({
 
     const state = reactive({
       type: null,
+      showConditions: false,
     })
 
     const refs = reactive({
@@ -57,19 +83,26 @@ export default defineComponent({
       xRange: [100, 1000],
     })
 
+    watch(() => state.showConditions, async (value) => {
+      await nextTick()
+      computedAxisInfo()
+    })
+
     onMounted(() => {
-      axisInfo.width = refs.axisSvgEl.width.baseVal.value
-      axisInfo.height = refs.axisSvgEl.height.baseVal.value
+      computedAxisInfo()
       window.onresize = function () {
-        axisInfo.width = refs.axisEl?.offsetWidth
-        axisInfo.height = refs.axisEl?.offsetHeight
+        computedAxisInfo()
       }
     })
 
     onUpdated(() => {
-      axisInfo.width = refs.axisEl?.offsetWidth
-      axisInfo.height = refs.axisEl?.offsetHeight
+      computedAxisInfo()
     })
+
+    function computedAxisInfo () {
+      axisInfo.width = refs.axisEl?.offsetWidth ?? 0
+      axisInfo.height = refs.axisEl?.offsetHeight ?? 0
+    }
 
     return {
       data,
@@ -77,6 +110,8 @@ export default defineComponent({
       ...toRefs(refs),
 
       axisInfo,
+
+      computedAxisInfo,
     }
   },
 })
